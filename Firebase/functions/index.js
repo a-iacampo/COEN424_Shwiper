cec5494feebeb5df40bd70daafcde9e8ac99cddc
Anthony I <a.iacampo99@gmail.com>
2020-11-14 23:33:13 -0500

    implemented a function to test kijiji scraper

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,9 +1,29 @@
 const functions = require('firebase-functions');
+const kijiji = require("kijiji-scraper");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// http request 1
+exports.testScraper = functions.https.onRequest((req, res) => {
+
+    const options = {
+        //Number of ads it fetches
+        minResults: 20
+    };
+
+    const params = {
+        locationId: kijiji.locations.QUEBEC.GREATER_MONTREAL,
+        categoryId: kijiji.categories.BUY_AND_SELL.CLOTHING,
+        sortByName: "priceAsc"
+    };
+
+    kijiji.search(params, options).then((ads) => {
+        // Use the ads array
+        for (let i = 0; i < ads.length; ++i) {
+            console.log(ads[i].description);
+        }
+        res.json(ads);
+    }).catch((error => {
+        console.log(error);
+    }));
+});
+
+//Run with Firebase serve
