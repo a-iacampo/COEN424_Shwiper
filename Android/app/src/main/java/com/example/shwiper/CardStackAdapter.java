9194acda08a859b5e2a6998f2ea6a6bf18703b88
JordanHum <jhum268@gmail.com>
2020-11-17 17:53:45 -0500

    basic implementation of android card stack view

---
+++ Android/app/src/main/java/com/example/shwiper/CardStackAdapter.java
@@ -0,0 +1,75 @@
+package com.example.shwiper;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CardStackAdapter extends RecyclerView.Adapter<CardStackAdapter.ViewHolder> {
+
+    private List<ItemModel> items;
+
+    public CardStackAdapter(List<ItemModel> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setData(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView title, location, description, price;
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.item_image);
+            title = itemView.findViewById(R.id.item_name);
+            location = itemView.findViewById(R.id.item_location);
+            description = itemView.findViewById(R.id.item_description);
+            price = itemView.findViewById(R.id.item_price);
+        }
+
+        void setData(ItemModel data) {
+            Picasso.get()
+                    .load(data.getImage())
+                    .fit()
+                    .centerCrop()
+                    .into(image);
+            title.setText(data.getTitle());
+            price.setText("$ " + data.getPrice());
+            location.setText(data.getLocation());
+            description.setText(data.getDescription());
+
+        }
+    }
+
+    public List<ItemModel> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemModel> items) {
+        this.items = items;
+    }
+}
