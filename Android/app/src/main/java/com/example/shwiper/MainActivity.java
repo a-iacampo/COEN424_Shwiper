a5b4001a0e1143bd63bfa64b47530270289be6d6
ltflynn <lthomas.flynn@gmail.com>
2020-11-25 20:17:36 -0500

    Fetching Kijiji Ads with FirebaseHelper

--- Android/app/src/main/java/com/example/shwiper/MainActivity.java
+++ Android/app/src/main/java/com/example/shwiper/MainActivity.java
@@ -8,12 +8,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DiffUtil;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +32,7 @@ import com.yuyakaido.android.cardstackview.Direction;
 import com.yuyakaido.android.cardstackview.StackFrom;
 import com.yuyakaido.android.cardstackview.SwipeableMethod;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,23 +45,59 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout drawer;
     private NavigationView navigationView;
     private Toolbar toolbar;
+    private ProgressBar progressBar;
 
     protected FirebaseHelper firebaseHelper;
+    //protected ArrayList<ItemModel> listFetchedOfAds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseHelper = new FirebaseHelper();
-
         initViews();
         setSupportActionBar(toolbar);
-
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,R.string.drawer_open, R.string.drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        initFirebaseHelper();
+
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void initViews() {
+        Log.d(TAG, "initViews:started");
+        drawer = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation_drawer);
+        toolbar = findViewById(R.id.toolbar);
+        progressBar = findViewById(R.id.progressBar);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initDrawerMenu(){
+
+    }
+
+    private void initFirebaseHelper(){
+        firebaseHelper = new FirebaseHelper();
+
+        //Attempts to fetch Kijiji Ads from Scraper
+        firebaseHelper.FectchFromScraper(new FirebaseHelper.FirebaseHelperCallback(){
+            @Override
+            public void onFetchAdsGot(ArrayList<ItemModel> items) {
+
+                progressBar.setVisibility(View.INVISIBLE);
+
+                initCardStack(items);
+            }
+        });
+
+
+    }
+
+    private void initCardStack(ArrayList<ItemModel> listFetchedOfAds){
         CardStackView cardStackView = findViewById(R.id.card_stack_view);
         manager = new CardStackLayoutManager(this, new CardStackListener() {
             @Override
@@ -78,7 +117,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
                 // Paginating
                 if (manager.getTopPosition() == adapter.getItemCount() - 5){
-                    paginate();
+                    paginate(listFetchedOfAds);
                 }
 
             }
@@ -97,6 +136,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             public void onCardAppeared(View view, int position) {
                 TextView tv = view.findViewById(R.id.item_name);
                 Log.d(TAG, "onCardAppeared: " + position + ", nama: " + tv.getText());
+
             }
 
             @Override
@@ -115,47 +155,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         manager.setCanScrollHorizontal(true);
         manager.setSwipeableMethod(SwipeableMethod.Manual);
         manager.setOverlayInterpolator(new LinearInterpolator());
-        adapter = new CardStackAdapter(addList());
+        adapter = new CardStackAdapter(listFetchedOfAds);
         cardStackView.setLayoutManager(manager);
         cardStackView.setAdapter(adapter);
         cardStackView.setItemAnimator(new DefaultItemAnimator());
 
     }
 
-    private void initViews() {
-        Log.d(TAG, "initViews:started");
-        drawer = findViewById(R.id.drawer);
-        navigationView = findViewById(R.id.navigation_drawer);
-        toolbar = findViewById(R.id.toolbar);
-
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    private void paginate() {
+    private void paginate(ArrayList<ItemModel> items) {
         List<ItemModel> old = adapter.getItems();
-        List<ItemModel> baru = new ArrayList<>(addList());
+        List<ItemModel> baru = new ArrayList<>(items);
         CardStackCallback callback = new CardStackCallback(old, baru);
         DiffUtil.DiffResult hasil = DiffUtil.calculateDiff(callback);
         adapter.setItems(baru);
         hasil.dispatchUpdatesTo(adapter);
-    }
-
-    private List<ItemModel> addList() {
-        List<ItemModel> items = new ArrayList<>();
-        items.add(new ItemModel("https://i.ebayimg.com/00/s/ODAwWDYwMA==/z/0FYAAOSwB65fst9t/$_57.JPG", "Title", "250.55", "Montreal", "Test of description"));
-
-        //firebaseHelper.FectchFromScraper(); //Call cloud function
-/*        items.add(new ItemModel(R.drawable.sample2, "Marpuah", "20", "Malang"));
-        items.add(new ItemModel(R.drawable.sample3, "Sukijah", "27", "Jonggol"));
-        items.add(new ItemModel(R.drawable.sample4, "Markobar", "19", "Bandung"));
-        items.add(new ItemModel(R.drawable.sample5, "Marmut", "25", "Hutan"));
-
-        items.add(new ItemModel(R.drawable.sample1, "Markonah", "24", "Jember"));
-        items.add(new ItemModel(R.drawable.sample2, "Marpuah", "20", "Malang"));
-        items.add(new ItemModel(R.drawable.sample3, "Sukijah", "27", "Jonggol"));
-        items.add(new ItemModel(R.drawable.sample4, "Markobar", "19", "Bandung"));
-        items.add(new ItemModel(R.drawable.sample5, "Marmut", "25", "Hutan"));*/
-        return items;
     }
 
     @Override
