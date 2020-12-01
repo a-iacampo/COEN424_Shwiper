5e9aac8795e17f69c5035c96fb43dd1e8da3bc39
Teng <tengzhao@live.ca>
2020-11-30 23:39:52 -0500

    Working Liked Ads List

--- Android/app/src/main/java/com/example/shwiper/adsLikedActivity.java
+++ Android/app/src/main/java/com/example/shwiper/adsLikedActivity.java
@@ -1,14 +1,61 @@
 package com.example.shwiper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class adsLikedActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class adsLikedActivity extends AppCompatActivity implements adsLikedListAdapter.OnLikedAdsListener {
+
+    protected RecyclerView recyclerView;
+    protected RecyclerView.Adapter mAdapter;
+    protected RecyclerView.LayoutManager layoutManager;
+    protected List<Ad> likedAdsList = new ArrayList<>();
+    private FirebaseHelper firebaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ads_liked);
+
+        firebaseHelper = new FirebaseHelper();
+        recyclerView = findViewById(R.id.recycle_view);
+        recyclerView.setHasFixedSize(false);
+
+        loadAds();
+    }
+
+    //Load all course from DB
+    public void loadAds(){
+
+        firebaseHelper.fetchLikedAds(new FirebaseHelper.FirebaseHelperCallback() {
+            @Override
+            public void onFetchAdsGot(List<Ad> items) {
+            }
+
+            @Override
+            public void onFetchLikedAds(List<Ad> items) {
+                likedAdsList = new ArrayList<>(items);
+                initRecycleView();
+            }
+
+        });
+    }
+
+    private void initRecycleView() {
+        layoutManager = new LinearLayoutManager(adsLikedActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new adsLikedListAdapter(adsLikedActivity.this, likedAdsList, adsLikedActivity.this);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onLikedAdsClick(int position) {
+
     }
 }
